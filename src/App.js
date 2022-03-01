7bc059516ca2c07c2428c493c7b5cefefcaca5b6
TegHocine <arcou98@hotmail.com>
2022-03-01 14:10:50 +0100

    feat: header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import Header from './components/header/Header'
 import ConfigRoutes from './configRoutes/ConfigRoutes'
+import Footer from './components/footer/Footer'
 
 import './app.scss'
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='*' element={<ConfigRoutes />} />
+        <Route
+          path='*'
+          element={
+            <>
+              <Header />
+              <ConfigRoutes />
+              <Footer />
+            </>
+          }
+        />
       </Routes>
     </BrowserRouter>
   )
