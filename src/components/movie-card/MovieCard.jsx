bade86f9d6ba9e991960489625bdf8f5e839660a
TegHocine <arcou98@hotmail.com>
2022-03-06 16:54:34 +0100

    style: scroll top when clicking on movie card

--- src/components/movie-card/MovieCard.jsx
+++ src/components/movie-card/MovieCard.jsx
@@ -4,9 +4,7 @@ import { Link } from 'react-router-dom'
 
 import { category } from '../../api/tmdbApi'
 import apiConfig from '../../api/apiConfig'
-
 import './movie-card.scss'
-// import play from '../../assets/play.svg'
 
 export default function MovieCard(props) {
   const item = props.item
@@ -16,10 +14,17 @@ export default function MovieCard(props) {
   const bg = apiConfig.w500Image(item.poster_path)
 
   return (
-    <Link to={link}>
-      <div className='movie-card' style={{ backgroundImage: `url(${bg})` }}>
-        {/* <img src={play} alt='play button' className='movie-card__button play' /> */}
-      </div>
+    <Link
+      to={link}
+      onClick={() =>
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth',
+        })
+      }>
+      <div
+        className='movie-card'
+        style={{ backgroundImage: `url(${bg})` }}></div>
       <h3 className='movie-card__title'>{item.title || item.name}</h3>
     </Link>
   )
