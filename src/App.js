633b398d9332e34a13ab389540bf5dd37ab8e662
TegHocine <arcou98@hotmail.com>
2022-04-01 23:35:52 +0100

    feat: delete footer component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Header from './components/header/Header'
 import ConfigRoutes from './configRoutes/ConfigRoutes'
-import Footer from './components/footer/Footer'
 
 import './app.scss'
 function App() {
